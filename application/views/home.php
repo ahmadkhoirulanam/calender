e19d756012fe0fa8438eb2472ce198f7343ca478
ahmad khoirul anam <karir.anam@gmail.com>
2023-03-29 10:33:56 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,80 @@
+<h3>PHP/MYSQL Calendar</h3>
+
+
+<table class="table">
+    <tr>
+        <td>
+            <div id="carousel-home" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="<?php echo base_url(); ?>assets/images/screenshot.png" alt="">
+
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo base_url(); ?>assets/images/screenshot1.png" alt="">
+
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo base_url(); ?>assets/images/screenshot2.png" alt="">
+
+                        <div class="carousel-caption">
+
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo base_url(); ?>assets/images/screenshot3.png" alt="">
+
+                        <div class="carousel-caption">
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+                <a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                </a>
+            </div>
+        </td>
+        <td>
+            <div>
+
+                <h4 class="label-text">Features</h4>
+                <ul>
+                    <li>Fully customizable</li>
+                    <li>Codeigniter framework</li>
+                    <li>Fullcalendar plugin by Arshaw</li>
+                    <li>MYSQL database</li>
+                </ul>
+            </div>
+            <div class="col-sm-6">								
+                <a href="" style="text-decoration: none !important">                            
+                <div class="p-3 bg-info-700 rounded overflow-hidden position-relative text-white mb-g">
+                    <div class="">
+                        <h3 class="display-4 d-block l-h-n m-0 fw-500">23 Siswa <small class="m-0 l-h-n">Jumlah Responden Quiz</small>
+                    </h3>
+                    </div>
+                    <i class="fal fa-file-alt position-absolute pos-right pos-bottom opacity-15 mb-n1 mr-n1" style="font-size:4rem"></i>
+                </div>
+                </a>
+            </div>
+
+        </td>
+    </tr>
+</table>
+<div>
---
+++ application/views/inc/tes.php
@@ -0,0 +1,29 @@
+<div id="js_demo_accordion-3a" class="collapse show" data-parent="#js_demo_accordion-3" style="">
+					<div class="card-body pb-0">
+						<div class="row">
+                            <div class="col-sm-6">								
+                                <a href="<?php echo baseurl("laporanM001"); ?>" style="text-decoration: none !important">                            
+                                <div class="p-3 bg-info-700 rounded overflow-hidden position-relative text-white mb-g">
+                                    <div class="">
+                                        <h3 class="display-4 d-block l-h-n m-0 fw-500"><?php echo $count;?> Siswa <small class="m-0 l-h-n">Jumlah Responden Quiz</small>
+                                    </h3>
+                                    </div>
+                                    <i class="fal fa-file-alt position-absolute pos-right pos-bottom opacity-15 mb-n1 mr-n1" style="font-size:4rem"></i>
+                                </div>
+                                </a>
+                            </div>
+                            <div class="col-sm-6">								
+                                <a href="#" style="text-decoration: none !important">                            
+                                <div class="p-3 bg-danger-500 rounded overflow-hidden position-relative text-white mb-g">
+                                    <div class="">
+                                        <h3 class="display-4 d-block l-h-n m-0 fw-500"><?php echo number_format($count2, 0, ',', '.');?> Sekolah<small class="m-0 l-h-n">Jumlah Sekolah</small>
+                                    </h3>
+                                    </div>
+                                    <i class="fal fa-file-alt position-absolute pos-right pos-bottom opacity-15 mb-n1 mr-n1" style="font-size:4rem"></i>
+                                </div>
+                                </a>
+                            </div>
+                           
+						</div>
+					</div>
+				</div>
